fff3f0b981d89f211b9a99f2fcc5f12e028991ea
souleyman <22018@supnum.mr>
2024-07-24 04:05:37 +0000

    Dv

---
+++ stage2/src/main/java/com/example/stage/stage/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.stage.stage.entity;
+
+import com.example.stage.stage.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String name;
+    private UserRole role;
+    @Lob
+    @Column(columnDefinition = "longblob")
+    private byte[] img;
+
+
+}
---
+++ stage2/src/main/java/com/example/stage/stage/services/auth/AuthServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.stage.stage.services.auth;
+
+import com.example.stage.stage.dto.SignupRequest;
+import com.example.stage.stage.dto.UserDto;
+import com.example.stage.stage.entity.User;
+import com.example.stage.stage.enums.UserRole;
+import com.example.stage.stage.repostory.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+    @Autowired
+    private UserRepository userRepository;
+
+    private  BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
+
+
+    public UserDto createUser(SignupRequest signupRequest) {
+        User user = new User();
+        user.setEmail(signupRequest.getEmail());
+        user.setName(signupRequest.getName());
+        user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
+        user.setRole(UserRole.ADMIN);
+        User createdUser = userRepository.save(user);
+
+        UserDto userDto = new UserDto();
+        userDto.setId(createdUser.getId());
+        return userDto;
+    }
+    public Boolean hasUserWithEmail(String email){
+        return userRepository.findFirstByEmail(email).isPresent();
+    }
+
+    @PostConstruct
+    public void createAdminAccount() {
+        User adminAccount =userRepository.findByRole(UserRole.ADMIN);
+        if (null == adminAccount) {
+            User user = new User();
+            user.setEmail("admin@test.com");
+            user.setName("admin");
+            user.setRole(UserRole.ADMIN);
+            user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+            userRepository.save(user);
+        }
+    }
+
+}
